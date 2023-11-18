354a17918d5d29aa40e624a0e143b8d6b90f0e76
PracticePenguin <sheng.yang@tum.de>
2023-11-18 23:49:08 +0100

    sql copy files and changes to spring project

--- hackatum23/src/main/java/com/example/hackatum23/serviceprovider/ServiceService.java
+++ hackatum23/src/main/java/com/example/hackatum23/serviceprovider/ServiceService.java
@@ -12,9 +12,9 @@ public class ServiceService {
         // Implement the logic to retrieve service providers based on the postal code
         // Example: return a list of service providers with matching postal codes
         // For simplicity, assume the postalCode is a property of ServiceProvider
-        return serviceProviderList.stream()
-            .filter(provider -> provider.getPostalCode().equals(postalCode))
-            .collect(Collectors.toList());
+        return null; //serviceProviderList.stream()
+            //.filter(provider -> provider.getPostalCode().equals(postalCode))
+           // .collect(Collectors.toList());
     }
 
 }
